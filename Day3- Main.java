76014162c1b7d578a046f838f7e2e3c301a02814
Lara-t56 <129784890+Lara-t56@users.noreply.github.com>
2023-04-05 15:52:43 +0300

    Add files via upload

---
+++ Day3- Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Q1-------------------------------------
+        
+//        for (int i=0; i<=100; i=i+2){
+//            System.out.println(i);
+//        }
+
+        //Q2-------------------------------------
+
+//        for (int i = 1; i < 100; i++) {
+//            if (i %2== 0) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+        //Q3--------------------------------------------
+
+//        for (int i=1; i<=10; i++){
+//            if (i>4){
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+       // Q4----------------------------------------
+
+//        String name = "Lara";
+//        for (int i=0; i < name.length();i++){
+//            System.out.println(name.charAt(i));
+//        }
+//
+        //Q5--------------------------------------
+
+//        String name ="Lara";
+//        for (int i=name.length()-1; i>=0; i--){
+//            System.out.println(name.charAt(i));
+//        }
+
+        //End-------------------------------------
+    }
+}
