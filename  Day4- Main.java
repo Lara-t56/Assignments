f06870a25a331a5e4b4a415a5928f837c1ed4c7e
Lara-t56 <129784890+Lara-t56@users.noreply.github.com>
2023-04-06 14:30:17 +0300

    Add files via upload

---
+++  Day4- Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Q1------------------------------------
+
+//        int[] num = {50, 60, 40, 10, 38, 65, 87, 56, 90, 100, 66, 80, 88, 98};
+//
+//        for (int x:num
+//             ) {
+//            if (  x>= 90 && x<= 100) {
+//                System.out.println("your grade is: A");
+//            } else if (x >= 80 && x >= 87) {
+//                System.out.println("your grade is: B");
+//            } else if (x >= 50 && x >= 38) {
+//                System.out.println("your grade is : F");
+//            } else {
+//                System.out.println(" your grade is : D");
+
+
+        // Q2---------------------------------------
+//        String[]name={"Shaza","Mohamed","Asmaa","Anfal","Yousra","Amin","Radwa","Yasmeen"};
+//        for (String l :name) {
+//            if (l.startsWith("A"))
+//                System.out.println(l);
+//        }
+//
+        //Q3----------------------------------------
+
+//        int[] number={50,60,40,10,38,65,87,56,90,100,66,80,88,40,98};
+//        for (int l:number){
+//            if (l>=50){
+//                System.out.println(l);
+//            }
+//        }
+
+        //Q4------------------------------------------------
+
+//        Scanner l = new Scanner(System.in);
+//        int[] numbre = {10, 20, 30, 40, 50, 100, 110, 120, 130, 140, 150, 200,};
+//        System.out.println("Enter your number:");
+//        int check = l.nextInt();
+//        for (int p:numbre){
+//            if (p == check) {
+//                System.out.println("The number is:" + p);
+//               break;
+//           }
+
+            // End---------------------------------------------------
+        }
+    }
+}
+
+
+
+
+
+
